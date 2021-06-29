cb33bc637cbbaa7f6d6ffb0e0261f7bad9c52214
kate litscher <kate@goodtime.io>
2021-06-29 11:01:57 -0400

    formatting args

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 set -e
 all_args=$@
 args_str=${all_args[9]}
-echo "${args//' '/'\" \"'}"
 formatted=$args_str | tr -s "\" \"" " "
 all_args[9]=$formatted
-
+echo ${all_args} 
 /dependency-check/bin/dependency-check.sh  ${all_args} 
